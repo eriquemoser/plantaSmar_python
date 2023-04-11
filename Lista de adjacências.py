f8ab4a15fc4c40914ebb93ee76a256eaa7083c70
Erique Moser <eriquemoser@yahoo.com.br>
2023-04-11 15:37:40 -0300

    identificação da falha
    melhorado um pouco mais o design do código e alarde de falha

--- Lista de adjacências.py
+++ Lista de adjacências.py
@@ -27,24 +27,28 @@ pos = 3
 
 #evolução do grafo de acordo com o evento informado pelo operador
 while True:
+    flag = 0
     print(f'estado atual: {pos}')
     print('eventos habilitados: ', end = '')
     leitura(grafo[pos])
-    #print(grafo[pos])
+
     var = str(input('Digite o evento desejado [0 p/ parar]: '))
     if var == '0':
         break
-    for elemento in grafo[pos]:
 
+    for elemento in grafo[pos]:
         if type(elemento) == list:
             if var in elemento:
-                #index = elemento.index(var)
-                #print(elemento[1])
                 pos = elemento[1]
-                break
+                flag=1
+
         else:
             if var in grafo[pos]:
-                #print(grafo[pos][1])
                 pos = grafo[pos][1]
+                flag=1
+
+    if flag==0:
+        print('FALHA NO SISTEMA DETECTADA!!!')
+        break
 
 
