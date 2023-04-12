a2a495186787826e3f04f13b95ae0d60d49a29dd
Erique Moser <eriquemoser@yahoo.com.br>
2023-04-12 10:05:16 -0300

    Programa que evolui a partir do txt

---
+++ leitura_automato/evolucao.py
@@ -0,0 +1,30 @@
+from time import sleep
+from leitura_automato.lib.interface import *
+from leitura_automato.lib.arquivo import *
+
+arq = 'automato.txt'
+if arquivoExiste(arq):
+    print('Arquivo encontrado!!!')
+else:
+    print('Arquivo não encontrado :(')
+    criararquivo(arq)
+
+pos = 0
+
+while True:
+    resposta = menu(['Ver eventos disponíveis', 'Digitar novo evento', 'Sair do sistema'],pos)
+    if resposta == 1:
+        lerarquivo(arq,pos)
+    elif resposta == 2:
+        cabecalho('NOVO EVENTO')
+        evento = int(input('Digite o número do evento: '))
+        post=evoluir(arq,pos,evento)
+        pos=post
+    elif resposta == 3:
+        cabecalho('Saindo do sistema... até logo')
+        break
+    else:
+        print('\033[31mERRO, opção inválida. Tente novamente.\033[m')
+    sleep(1)
+
+
