0a13f117e17ac21fae507a9f5f38d70920ae0e7e
Erique Moser <eriquemoser@yahoo.com.br>
2023-04-19 10:48:03 -0300

    adicionei a chave no opc

--- opc.py
+++ opc.py
@@ -12,12 +12,17 @@ opc = OpenOPC.client()
 
 opc.connect('Smar.DfiOleServer.0', 'localhost')
 
-tags = ['LT_1_AI.OUT.VALUE'] #lista ou tupla
+tags = ['LT_1_AI.OUT.VALUE','FB_MDI.OUT_D3.VALUE'] #lista ou tupla
 
 while True:
 
     alarmes = opc.read(tags, group='Group_1', update=1)
 
-    print(alarmes)
+    #print(alarmes)
+    #print(type(alarmes))
+    #print(alarmes[0][1])
+    #print(alarmes[1])
+    print(f'nível: {alarmes[0][1]:.2f}')
+    print(f'chave: {alarmes[1][1]}')
 
     time.sleep(1)
