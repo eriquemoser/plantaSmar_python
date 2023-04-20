6033f13ce0100ae641b1b1a53cfd93edd294d7a5
Erique Moser <eriquemoser@yahoo.com.br>
2023-04-20 10:48:48 -0300

    DETECTOR DE EVENTOS

--- opc.py
+++ opc.py
@@ -1,28 +1,43 @@
 import pywintypes
-
 import OpenOPC
-
 import time
 
-
-
 pywintypes.datetime = pywintypes.TimeType
-
 opc = OpenOPC.client()
-
 opc.connect('Smar.DfiOleServer.0', 'localhost')
+tags = ['FB_MDI.OUT_D1.VALUE','FB_MDI.OUT_D2.VALUE','FB_MDI.OUT_D3.VALUE','FB_MDI.OUT_D4.VALUE',
+        'FB_MDI.OUT_D5.VALUE','FB_MDI.OUT_D6.VALUE','FB_MDI.OUT_D7.VALUE','FB_MDI.OUT_D8.VALUE',
 
-tags = ['LT_1_AI.OUT.VALUE','FB_MDI.OUT_D3.VALUE'] #lista ou tupla
+        'FFB_MDI.OUT_D1.VALUE','FFB_MDI.OUT_D2.VALUE','FFB_MDI.OUT_D3.VALUE','FFB_MDI.OUT_D4.VALUE',
+        'FFB_MDI.OUT_D5.VALUE','FFB_MDI.OUT_D6.VALUE','FFB_MDI.OUT_D7.VALUE','FFB_MDI.OUT_D8.VALUE',
 
+        'FFFB_MDI.OUT_D1.VALUE','FFFB_MDI.OUT_D2.VALUE'
+        ] #lista ou tupla
+
+#INICIALIZAÇÃO
+cont = 0
+init = []
+nomes= ['$$$$', 'c_d_lo', 'chave2', 'c_d_hi', 'c_d_sp', 'c_u_hi', 'c_u_hi_hi', 'c_u_lo', 'c_u_sp',
+        'c_underflow', 'c_overflow', 'start', 'stop', 'bligada', 'bdesligada', 'vfecha', 'vabre',
+        'vregula']
+
+#CAPTURA OS ESTADOS INICIAIS
+alarmes = opc.read(tags, group='Group_1', update=1)
+for c in range(0,len(alarmes)):
+    init.append(alarmes[c][1])
+print(init)
+
+#CAPTURA DOS DADOS
 while True:
-
     alarmes = opc.read(tags, group='Group_1', update=1)
+    for c in range(0, len(alarmes)):
+        if alarmes[c][1] != init[c]:
+            print(nomes[c])
+            #print(alarmes[c][0])
+            init[c]=alarmes[c][1]
 
-    #print(alarmes)
-    #print(type(alarmes))
-    #print(alarmes[0][1])
-    #print(alarmes[1])
-    print(f'nível: {alarmes[0][1]:.2f}')
-    print(f'chave: {alarmes[1][1]}')
+    print(f'{cont} -------------------------------')
+    cont+=1
 
-    time.sleep(1)
+
+time.sleep(0.1)
